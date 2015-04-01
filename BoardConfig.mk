e750ce818f2b4dcbe62b12012082a0aaf19efca5
Stephen Smalley <sds@tycho.nsa.gov>
2015-04-01 10:30:09 -0400

    ferrari: Drop BOARD_SEPOLICY_UNION.
    As suggested in the comments on
    https://android-review.googlesource.com/#/c/141560/
    drop BOARD_SEPOLICY_UNION and simplify the build_policy logic.
    Union all files found under BOARD_SEPOLICY_DIRS.
    
    Change-Id: I4214893c999c23631f5456cb1b8edd59771ef13b
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,23 +184,7 @@ ADD_RADIO_FILES := true
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/xiaomi/ferrari/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    bluetooth_loader.te \
-    bootanim.te \
-    file.te \
-    file_contexts \
-    healthd.te \
-    property_contexts \
-    qseecomd.te \
-    surfaceflinger.te \
-    system_app.te \
-    system_server.te \
-    system.te \
-    wcnss_service.te
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Qualcomm support
 BOARD_USES_QC_TIME_SERVICES := true
